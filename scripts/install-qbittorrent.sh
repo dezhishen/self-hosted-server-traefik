9fae026cfda6f84472ed9ef7ab4cef78a4129519
我怎么就不是一只猫呢? <26274059+dezhishen@users.noreply.github.com>
2024-12-17 21:24:47 +0800

    feat:新增transmission创建

--- scripts/install-qbittorrent.sh
+++ scripts/install-qbittorrent.sh
@@ -5,24 +5,25 @@ docker_network_name=$3
 tls=$4
 image=linuxserver/qbittorrent
 port=8080
-docker_container_name=qbittorrent
+container_name=qbittorrent
 #`dirname $0`/create-dir.sh $base_data_dir/qbittorrent
 #`dirname $0`/create-dir.sh $base_data_dir/qbittorrent/config
 docker pull ${image}
 
-`dirname $0`/stop-container.sh ${docker_container_name}
+`dirname $0`/stop-container.sh ${container_name}
 
-docker run -d --name=${docker_container_name} \
+docker run -d --name=${container_name} \
 --restart=always \
 -m 1G \
 --network=host \
 -e TZ="Asia/Shanghai" \
 -e LANG="zh_CN.UTF-8" \
 -e PUID=`id -u` -e PGID=`id -g` \
--v $base_data_dir/${docker_container_name}/config:/config \
+-v $base_data_dir/${container_name}/config:/config \
 -v $base_data_dir/public/downloads:/data/downloads \
--v $base_data_dir/${docker_container_name}/incomplete-torrents:/incomplete-torrents \
--v $base_data_dir/${docker_container_name}/finished-torrents:/finished-torrents \
+-v $base_data_dir/${container_name}/incomplete-torrents:/incomplete-torrents \
+-v $base_data_dir/${container_name}/finished-torrents:/finished-torrents \
 ${image}
+`dirname $0`/create-traefik-provider.sh $domain $base_data_dir $docker_network_name $tls $container_name $port
 
 
