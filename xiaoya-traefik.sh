92c46b24f5f6e288716dab9f8c447d5655c6c523
dezhishen <26274059+dezhishen@users.noreply.github.com>
2024-12-20 16:51:03 +0800

    feat:增加xiaoya的traefik配置文件脚本

---
+++ xiaoya-traefik.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+domain=`./scripts/get-args.sh domain "主域名(如 baidu.com或者app.baidu.com)"`
+if [ -z "$domain" ]; then
+    read -p "请输入主域名(如 baidu.com或者app.baidu.com):" domain
+    if [ -z "$domain" ]; then
+        echo "主域名不能为空"
+        exit 0
+    else
+        ./scripts/set-args.sh domain $domain
+    fi
+fi
+base_data_dir=`./scripts/get-args.sh base_data_dir "数据目录(如 /docker_data)"`
+if [ -z "$base_data_dir" ]; then
+    read -p "请输入数据目录(如 /docker_data):" base_data_dir
+    if [ -z "$base_data_dir" ]; then
+        echo "数据目录为空,使用默认值 /docker_data"
+        base_data_dir=/docker_data
+    fi
+    ./scripts/set-args.sh base_data_dir $base_data_dir
+    echo "文件不存在，创建文件且设置权限"
+    sudo mkdir -p $base_data_dir
+    sudo chown -R `id -u`:`id -g` $base_data_dir
+fi
+
+docker_network_name=`./scripts/get-args.sh docker_network_name "Docker网络名称(如 traefik)"`
+
+if [ -z "$docker_network_name" ]; then
+    read -p "请输入Docker网络名称(如 traefik):" docker_network_name
+    if [ -z "$docker_network_name" ]; then
+        echo "Docker网络名称为空,使用默认值 traefik"
+        docker_network_name=traefik
+    fi
+    ./scripts/set-args.sh docker_network_name $docker_network_name
+fi
+tls=`./scripts/get-args.sh tls "是否开启ssl[true/false]"`
+if [ -z "$tls" ]; then
+    read -p "是否开启ssl[true/false]:" tls
+    if [ -z "$tls" ]; then
+        echo "是否开启ss为空,使用默认值 true"
+        tls=true
+    fi
+    ./scripts/set-args.sh tls $tls
+fi
+
+read -p "是否创建xiaoya-alist的traefik配置文件[y/n]:" create_xiaoya_alist_traefik_config
+if [ "$create_xiaoya_alist_traefik_config" == "y" ]; then
+    sh ./scripts/create-traefik-provider.sh xiaoya-alist $base_data_dir $docker_network_name $tls xiaoya-alist 5244
+fi
+
+read -p "是否创建xiaoya-emby的traefik配置文件[y/n]:" create_xiaoya_emby_traefik_config
+if [ "$create_xiaoya_emby_traefik_config" == "y" ]; then
+    sh ./scripts/create-traefik-provider.sh xiaoya-emby $base_data_dir $docker_network_name $tls xiaoya-emby 2345
+fi
+
