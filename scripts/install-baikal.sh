5a9856e0f206c9be7477d7e32b8fce8dd4c31c98
我怎么就不是一只猫呢? <26274059+dezhishen@users.noreply.github.com>
2025-01-21 21:57:51 +0800

    mod:修改baikal安装脚本

--- scripts/install-baikal.sh
+++ scripts/install-baikal.sh
@@ -47,20 +47,29 @@ docker run --name=${container_name} \
 ${image}
 
 # 是否安装infcloud
-read -p "是否安装agendav(y/n):" yN
+read -p "是否安装infcloud(y/n):" yN
 case $yN in
     [Yy]* )
-      container_name=agendav
-      image=ghcr.io/nagimov/agendav-docker:latest
-      port=8080
-      AGENDAV_ENC_KEY=$(`dirname $0`/get-args.sh AGENDAV_ENC_KEY php加密key)
-      if [ -z "$AGENDAV_ENC_KEY" ]; then
-          read -p "请输入php加密key:" AGENDAV_ENC_KEY
-          if [ -z "$AGENDAV_ENC_KEY" ]; then
-              echo "随机生成php加密key"
-              AGENDAV_ENC_KEY=`$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)`
-          fi
-          `dirname $0`/set-args.sh AGENDAV_ENC_KEY "$AGENDAV_ENC_KEY"
+      # 安装 php-fpm
+      container_name=infcloud-php
+      image=php:7.3-fpm-alpine
+
+      docker pull ${image}
+      `dirname $0`/stop-container.sh ${container_name}
+      docker run --name=${container_name} \
+      --restart=always -d -m 128M \
+      -v ${base_data_dir}/${container_name}/nginx:/usr/share/nginx/infcloud:ro \
+      ${image}
+
+      # 安装infcloud
+      container_name=infcloud
+      image=infcloud/infcloud:latest
+      port=80
+      # 如果文件 ${base_data_dir}/${container_name}/config-baikal.js 不存在，则下载 
+      if [ ! -f "${base_data_dir}/${container_name}/config-baikal.js" ]; then
+        wget -O ${base_data_dir}/${container_name}/config-baikal.js https://raw.githubusercontent.com/ckulka/infcloud-docker/refs/heads/master/examples/config-baikal.js
+        # 修改 baikal.localhost => baikal.$domain
+        sed -i "s/baikal.localhost/baikal.$domain/g" ${base_data_dir}/${container_name}/config-baikal.js
       fi
       docker pull ${image}
       `dirname $0`/stop-container.sh ${container_name}
@@ -68,15 +77,8 @@ case $yN in
       --restart=always -d -m 128M \
       -e TZ="Asia/Shanghai" \
       --network=$docker_network_name --network-alias=${container_name} --hostname=${container_name} \
-      -e AGENDAV_SERVER_NAME=127.0.0.1 \
-      -e AGENDAV_TITLE="AgendaV" \
-      -e AGENDAV_FOOTER="host by $domain" \
-      -e AGENDAV_ENC_KEY=${AGENDAV_ENC_KEY} \
-      -e AGENDAV_CALDAV_SERVER=http://baikal/cal.php \
-      -e AGENDAV_CALDAV_PUBLIC_URL=https://baikal.$domain \
-      -e AGENDAV_TIMEZONE=Asia/Shanghai \
-      -e AGENDAV_LANG=zh_CN \
-      -e AGENDAV_LOG_DIR=/tmp/ \
+      -v ${base_data_dir}/${container_name}/nginx:/usr/share/nginx/infcloud \
+      -v ${base_data_dir}/${container_name}/config-baikal.js:/usr/share/nginx/infcloud/config.js:ro \
       --label "traefik.enable=true" \
       --label 'traefik.http.routers.'${container_name}'.rule=Host(`'${container_name}.$domain'`)' \
       --label "traefik.http.routers.${container_name}.tls=${tls}" \
@@ -86,3 +88,43 @@ case $yN in
       ${image}
     ;;
 esac
+
+# read -p "是否安装agendav(y/n):" yN
+# case $yN in
+#     [Yy]* )
+#       container_name=agendav
+#       image=ghcr.io/nagimov/agendav-docker:latest
+#       port=8080
+#       AGENDAV_ENC_KEY=$(`dirname $0`/get-args.sh AGENDAV_ENC_KEY php加密key)
+#       if [ -z "$AGENDAV_ENC_KEY" ]; then
+#           read -p "请输入php加密key:" AGENDAV_ENC_KEY
+#           if [ -z "$AGENDAV_ENC_KEY" ]; then
+#               echo "随机生成php加密key"
+#               AGENDAV_ENC_KEY=`$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)`
+#           fi
+#           `dirname $0`/set-args.sh AGENDAV_ENC_KEY "$AGENDAV_ENC_KEY"
+#       fi
+#       docker pull ${image}
+#       `dirname $0`/stop-container.sh ${container_name}
+#       docker run --name=${container_name} \
+#       --restart=always -d -m 128M \
+#       -e TZ="Asia/Shanghai" \
+#       --network=$docker_network_name --network-alias=${container_name} --hostname=${container_name} \
+#       -e AGENDAV_SERVER_NAME=127.0.0.1 \
+#       -e AGENDAV_TITLE="AgendaV" \
+#       -e AGENDAV_FOOTER="host by $domain" \
+#       -e AGENDAV_ENC_KEY=${AGENDAV_ENC_KEY} \
+#       -e AGENDAV_CALDAV_SERVER=http://baikal/cal.php \
+#       -e AGENDAV_CALDAV_PUBLIC_URL=https://baikal.$domain \
+#       -e AGENDAV_TIMEZONE=Asia/Shanghai \
+#       -e AGENDAV_LANG=zh_CN \
+#       -e AGENDAV_LOG_DIR=/tmp/ \
+#       --label "traefik.enable=true" \
+#       --label 'traefik.http.routers.'${container_name}'.rule=Host(`'${container_name}.$domain'`)' \
+#       --label "traefik.http.routers.${container_name}.tls=${tls}" \
+#       --label "traefik.http.routers.${container_name}.tls.certresolver=traefik" \
+#       --label "traefik.http.routers.${container_name}.tls.domains[0].main=*.$domain" \
+#       --label "traefik.http.services.${container_name}.loadbalancer.server.port=${port}" \
+#       ${image}
+#     ;;
+# esac
