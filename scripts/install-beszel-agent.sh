1d19e8d25006c3a22e188fe3b6d05f1bbc4c17ee
dezhishen <26274059+dezhishen@users.noreply.github.com>
2025-02-18 10:44:01 +0800

    mod:文件权限差异

--- scripts/install-beszel-agent.sh
+++ scripts/install-beszel-agent.sh
@@ -3,28 +3,28 @@ domain=$1
 base_data_dir=$2
 docker_network_name=$3
 tls=$4
-container_name=cloudbak
-image=likeflyme/cloudbak
+container_name=beszel-agent
+image=henrygd/beszel-agent
+port=45876
 
-docker pull ${image}
-port=9527
+docker pull $image
+
 `dirname $0`/stop-container.sh ${container_name}
 
-docker run  \
---hostname ${container_name} \
---name=${container_name} \
---restart=always -d \
--e PUID=`id -u` -e PGID=`id -g` \
--m 512M \
+docker run -d --name=${container_name} \
+--restart=always \
 --network=$docker_network_name \
 --network-alias=${container_name} \
 -e TZ="Asia/Shanghai" \
 -e LANG="zh_CN.UTF-8" \
--v $base_data_dir/${container_name}/data:/app/data \
+-e PORT=45876 \
+-e KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBW46jEI5AIRIVTNYTk1bnCWXQplc0EtHxFfWMroSXE" \
+-m 64M \
+-v /var/run/docker.sock:/var/run/docker.sock:ro \
 --label 'traefik.http.routers.'${container_name}'.rule=Host(`'${container_name}.$domain'`)' \
 --label "traefik.http.routers.${container_name}.tls=${tls}" \
 --label "traefik.http.routers.${container_name}.tls.certresolver=traefik" \
 --label "traefik.http.routers.${container_name}.tls.domains[0].main=*.$domain" \
 --label "traefik.http.services.${container_name}.loadbalancer.server.port=${port}" \
 --label "traefik.enable=true" \
-${image}
+$image
