75d57c7edc1252cccad5aa5cda82acaa5c3c4719
shendezhi <shendz@cecinvestment.com>
2025-02-18 10:44:01 +0800

    mod:文件权限差异

--- scripts/install-beszel.sh
+++ scripts/install-beszel.sh
@@ -3,28 +3,27 @@ domain=$1
 base_data_dir=$2
 docker_network_name=$3
 tls=$4
-container_name=cloudbak
-image=likeflyme/cloudbak
+container_name=beszel
+image=henrygd/beszel
+port=8090
 
-docker pull ${image}
-port=9527
+docker pull $image
+
 `dirname $0`/stop-container.sh ${container_name}
 
-docker run  \
---hostname ${container_name} \
---name=${container_name} \
---restart=always -d \
--e PUID=`id -u` -e PGID=`id -g` \
--m 512M \
+docker run -d --name=${container_name} \
+--restart=always \
 --network=$docker_network_name \
 --network-alias=${container_name} \
 -e TZ="Asia/Shanghai" \
 -e LANG="zh_CN.UTF-8" \
--v $base_data_dir/${container_name}/data:/app/data \
+-e PUID=`id -u` -e PGID=`id -g` \
+-m 64M \
+-v $base_data_dir/${container_name}/data:/beszel_data \
 --label 'traefik.http.routers.'${container_name}'.rule=Host(`'${container_name}.$domain'`)' \
 --label "traefik.http.routers.${container_name}.tls=${tls}" \
 --label "traefik.http.routers.${container_name}.tls.certresolver=traefik" \
 --label "traefik.http.routers.${container_name}.tls.domains[0].main=*.$domain" \
 --label "traefik.http.services.${container_name}.loadbalancer.server.port=${port}" \
 --label "traefik.enable=true" \
-${image}
+$image
