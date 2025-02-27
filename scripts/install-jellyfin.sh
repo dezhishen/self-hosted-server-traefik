aa0d05f136be60c70668650cd22f194ea23d61d5
dezhishen <26274059+dezhishen@users.noreply.github.com>
2025-02-27 17:35:07 +0800

    mod:修改jellyfin的脚本，增加jellysearch安装

--- scripts/install-jellyfin.sh
+++ scripts/install-jellyfin.sh
@@ -32,3 +32,42 @@ docker run  \
 --label "traefik.http.services.${container_name}.loadbalancer.server.port=${port}" \
 --label "traefik.enable=true" \
 ${image}
+
+# 是否安装jellysearch
+read -p "是否安装jellysearch? [y/n] " install_jellysearch
+if [ "$install_jellysearch" == "y" ]; then
+    MEILI_MASTER_KEY=$(`dirname $0`/get-args.sh MEILI_MASTER_KEY MasterKey)
+    if [ -z "$MEILI_MASTER_KEY" ]; then
+        read -p "请输入MasterKey:" MEILI_MASTER_KEY
+        if [ -z "$MEILI_MASTER_KEY" ]; then
+            echo "随机生成MasterKey"
+            MEILI_MASTER_KEY="$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)"
+            echo "随机MasterKey为：${MEILI_MASTER_KEY}"
+        fi
+        `dirname $0`/set-args.sh MEILI_MASTER_KEY "$MEILI_MASTER_KEY"
+    fi
+
+    jellyfin_container_name=${container_name}
+    container_name=jellysearch
+    image=domistyle/jellysearch
+    port=5000
+    docker run --restart=always -d \
+    --hostname ${container_name} --network=$docker_network_name --network-alias=${container_name} \
+    -e PUID=`id -u` -e PGID=`id -g` \
+    -e MEILI_MASTER_KEY=${MEILI_MASTER_KEY} \
+    -e INDEX_CRON="0 0 0/2 ? * * *" \
+    --user=`id -u`:`id -g` \
+    --name=${container_name} \
+    --network=$docker_network_name \
+    --network-alias=${container_name} \
+    -e TZ="Asia/Shanghai" \
+    -e LANG="zh_CN.UTF-8" \
+    -v $base_data_dir/jellyfin/config:/config \
+    --label 'traefik.http.routers.'${container_name}'.rule=Host(`'${jellyfin_container_name}.$domain'`) && ( QueryRegexp(`searchTerm`, `(.*?)`) || QueryRegexp(`SearchTerm`, `(.*?)`))' \
+    --label "traefik.http.routers.${container_name}.tls=${tls}" \
+    --label "traefik.http.routers.${container_name}.tls.certresolver=traefik" \
+    --label "traefik.http.routers.${container_name}.tls.domains[0].main=*.$domain" \
+    --label "traefik.http.services.${container_name}.loadbalancer.server.port=${port}" \
+    --label "traefik.enable=true" \
+    ${image}
+fi
