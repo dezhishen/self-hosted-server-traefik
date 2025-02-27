9528521ad4aff5b299983890d56e3bc78ae42fef
dezhishen <26274059+dezhishen@users.noreply.github.com>
2025-02-27 17:38:34 +0800

    fix:安装代码错误

--- scripts/install-jellyfin.sh
+++ scripts/install-jellyfin.sh
@@ -6,36 +6,39 @@ tls=$4
 container_name=jellyfin
 image=linuxserver/jellyfin:latest
 port=8096
+# 是否重装jellyfin
+read -p "是否重装jellyfin? [y/n] " reinstall
+if [ "$reinstall" = "y" ]; then
 
-docker pull ${image}
-`dirname $0`/stop-container.sh ${container_name}
-video_gid=$(cat /etc/group | grep -e video | cut -d ":" -f 3)
-render_gid=$(cat /etc/group | grep -e render | cut -d ":" -f 3)
-docker run  \
---hostname ${container_name} \
---privileged --restart=always -d \
---device /dev/dri \
--e PUID=`id -u` -e PGID=`id -g` \
---name=${container_name} \
--m 1024M \
---network=$docker_network_name \
---network-alias=${container_name} \
--e TZ="Asia/Shanghai" \
--e LANG="zh_CN.UTF-8" \
--v $base_data_dir/jellyfin/config:/config \
--v $base_data_dir/public/:/data \
--p ${port}:${port} \
---label 'traefik.http.routers.'${container_name}'.rule=Host(`'${container_name}.$domain'`)' \
---label "traefik.http.routers.${container_name}.tls=${tls}" \
---label "traefik.http.routers.${container_name}.tls.certresolver=traefik" \
---label "traefik.http.routers.${container_name}.tls.domains[0].main=*.$domain" \
---label "traefik.http.services.${container_name}.loadbalancer.server.port=${port}" \
---label "traefik.enable=true" \
-${image}
-
+    docker pull ${image}
+    `dirname $0`/stop-container.sh ${container_name}
+    video_gid=$(cat /etc/group | grep -e video | cut -d ":" -f 3)
+    render_gid=$(cat /etc/group | grep -e render | cut -d ":" -f 3)
+    docker run  \
+    --hostname ${container_name} \
+    --privileged --restart=always -d \
+    --device /dev/dri \
+    -e PUID=`id -u` -e PGID=`id -g` \
+    --name=${container_name} \
+    -m 1024M \
+    --network=$docker_network_name \
+    --network-alias=${container_name} \
+    -e TZ="Asia/Shanghai" \
+    -e LANG="zh_CN.UTF-8" \
+    -v $base_data_dir/jellyfin/config:/config \
+    -v $base_data_dir/public/:/data \
+    -p ${port}:${port} \
+    --label 'traefik.http.routers.'${container_name}'.rule=Host(`'${container_name}.$domain'`)' \
+    --label "traefik.http.routers.${container_name}.tls=${tls}" \
+    --label "traefik.http.routers.${container_name}.tls.certresolver=traefik" \
+    --label "traefik.http.routers.${container_name}.tls.domains[0].main=*.$domain" \
+    --label "traefik.http.services.${container_name}.loadbalancer.server.port=${port}" \
+    --label "traefik.enable=true" \
+    ${image}
+fi
 # 是否安装jellysearch
 read -p "是否安装jellysearch? [y/n] " install_jellysearch
-if [ "$install_jellysearch" == "y" ]; then
+if [ "$install_jellysearch" = "y" ]; then
     MEILI_MASTER_KEY=$(`dirname $0`/get-args.sh MEILI_MASTER_KEY MasterKey)
     if [ -z "$MEILI_MASTER_KEY" ]; then
         read -p "请输入MasterKey:" MEILI_MASTER_KEY
@@ -51,7 +54,7 @@ if [ "$install_jellysearch" == "y" ]; then
     container_name=jellysearch
     image=domistyle/jellysearch
     port=5000
-    docker run --restart=always -d \
+    docker run --restart=always -d -m 128M \
     --hostname ${container_name} --network=$docker_network_name --network-alias=${container_name} \
     -e PUID=`id -u` -e PGID=`id -g` \
     -e MEILI_MASTER_KEY=${MEILI_MASTER_KEY} \
