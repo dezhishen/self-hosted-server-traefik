f69318a784a9cfc0a4d5f95a52db3dc74782cf06
dezhishen <26274059+dezhishen@users.noreply.github.com>
2025-04-09 15:16:57 +0800

    feat:增加mariadb备份程序

--- scripts/install-mariadb.sh
+++ scripts/install-mariadb.sh
@@ -14,31 +14,60 @@ if [ -z "$MYSQL_PASSWORD" ]; then
         MARIADB_PASSWORD=`$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)`  
     fi                                                                                         
     `dirname $0`/set-args.sh MYSQL_PASSWORD "$MYSQL_PASSWORD"                          
-fi                                                                                             
-MYSQL_PORT_MAPPING=$(`dirname $0`/get-args.sh MYSQL_PORT_MAPPING 是否映射端口[y/n])
-if [ -z "$MYSQL_PORT_MAPPING" ]; then                                                          
-    read -p "是否映射端口[y/n]:" MYSQL_PORT_MAPPING                                                        
-    if [ -z "$MYSQL_PORT_MAPPING" ]; then                                                      
-        echo "是否映射端口[y/n]为空，默认不映射端口"
-        MYSQL_PORT_MAPPING="n"                                                                       
-    fi                                                                                         
-    `dirname $0`/set-args.sh MYSQL_PORT_MAPPING "$MYSQL_PORT_MAPPING"                          
-fi    
-docker pull $image
-docker stop $container_name > /dev/null
-docker rm $container_name
-docker run --restart=always -d --name ${container_name} -m 512M \
---user=`id -u`:`id -g` \
--e TZ=Asia/Shanghai \
--e LANG=zh_CN.UTF-8 \
--e MARIADB_USER=root \
--e MARIADB_ROOT_PASSWORD=${MYSQL_PASSWORD} \
-`if [ $MYSQL_PORT_MAPPING = "y" ]; then echo "-p ${port}:${port}"; fi` \
--v ${base_data_dir}/${container_name}/data:/var/lib/mysql:Z \
---network=${docker_network_name} --network-alias=${container_name} \
-${image}
+fi  
+read -p "是否重装 mariadb ? [y/n] " install_mariadb
+if [ "$install_mariadb" = "y" ]; then
+    MYSQL_PORT_MAPPING=$(`dirname $0`/get-args.sh MYSQL_PORT_MAPPING 是否映射端口[y/n])
+    if [ -z "$MYSQL_PORT_MAPPING" ]; then                                                          
+        read -p "是否映射端口[y/n]:" MYSQL_PORT_MAPPING                                                        
+        if [ -z "$MYSQL_PORT_MAPPING" ]; then                                                      
+            echo "是否映射端口[y/n]为空，默认不映射端口"
+            MYSQL_PORT_MAPPING="n"                                                                       
+        fi                                                                                         
+        `dirname $0`/set-args.sh MYSQL_PORT_MAPPING "$MYSQL_PORT_MAPPING"                          
+    fi    
+    docker pull $image
+    docker stop $container_name > /dev/null
+    docker rm $container_name
+    docker run --restart=always -d --name ${container_name} -m 512M \
+    --user=`id -u`:`id -g` \
+    -e TZ=Asia/Shanghai \
+    -e LANG=zh_CN.UTF-8 \
+    -e MARIADB_USER=root \
+    -e MARIADB_ROOT_PASSWORD=${MYSQL_PASSWORD} \
+    `if [ $MYSQL_PORT_MAPPING = "y" ]; then echo "-p ${port}:${port}"; fi` \
+    -v ${base_data_dir}/${container_name}/data:/var/lib/mysql:Z \
+    --network=${docker_network_name} --network-alias=${container_name} \
+    ${image}
+    echo "设置mysql_host为${container_name}"
+    `dirname $0`/set-args.sh MYSQL_HOST "${container_name}"
+    echo "设置mysql_port为${port}"
+    `dirname $0`/set-args.sh MYSQL_PORT "${port}"
+fi
+mariadb_container_name=${container_name}
 
-echo "设置mysql_host为${container_name}"
-`dirname $0`/set-args.sh MYSQL_HOST "${container_name}"
-echo "设置mysql_port为${port}"
-`dirname $0`/set-args.sh MYSQL_PORT "${port}"
+read -p "是否重装 备份程序 ? [y/n] " install_mariadb_backup
+if [ "$install_mariadb_backup" = "y" ]; then
+    MYSQL_HOST=${mariadb_container_name}
+    MYSQL_PASSWORD=$(`dirname $0`/get-args.sh MYSQL_PASSWORD "mysql密码" )
+    container_name=mariadb-backup
+    image=databack/mysql-backup
+    docker pull $image
+    docker stop $container_name > /dev/null
+    docker rm $container_name
+    docker run --restart=always -d \
+    --hostname=${container_name} \
+    --name ${container_name} -m 64M \
+    --user=`id -u`:`id -g` \
+    -e TZ=Asia/Shanghai \
+    -e LANG=zh_CN.UTF-8 \
+    -e DB_SERVER=${MYSQL_HOST} \
+    -e DB_USER=root \
+    -e DB_PASS=${MYSQL_PASSWORD} \
+    -e DB_DUMP_FREQUENCY=60 \
+    -e DB_DUMP_BEGIN=0000 \
+    -e DB_DUMP_TARGET=/db \
+    -v ${base_data_dir}/${container_name}/backup/:/db \
+    --network=${docker_network_name} --network-alias=${container_name} \
+    ${image} dump
+fi
