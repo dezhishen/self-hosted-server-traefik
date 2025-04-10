4a871e3614b14be7bc9ec3fa61603127ecec5033
dezhishen <26274059+dezhishen@users.noreply.github.com>
2025-04-10 13:51:42 +0800

    mod:增加语言资源修复的初始化脚本

--- scripts/install-lidarr.sh
+++ scripts/install-lidarr.sh
@@ -9,7 +9,14 @@ image=linuxserver/lidarr:latest
 
 docker pull ${image}
 `dirname $0`/stop-container.sh ${container_name}
-
+mkdir -p $base_data_dir/${container_name}/init/init-lidarr-resource
+echo """
+#!/usr/bin/with-contenv bash
+# shellcheck shell=bash
+ln -s /app/lidarr/bin/Localization/Core/zh.json/zh_CN.json /app/lidarr/bin/Localization/Core/zh.json/zh.json > /dev/null
+""" > $base_data_dir/${container_name}/init/init-lidarr-resource/run
+echo "oneshot" > $base_data_dir/${container_name}/init/init-lidarr-resource/type
+echo "up" > /etc/s6-overlay/s6-rc.d/init-lidarr-resource/run
 docker run -d --name=${container_name} \
 --restart=always \
 -m 512M --memory-swap=1G \
@@ -19,6 +26,7 @@ docker run -d --name=${container_name} \
 -e LANG="zh_CN.UTF-8" \
 -e PUID=`id -u` -e PGID=`id -g` \
 -v $base_data_dir/${container_name}/config:/config \
+-v $base_data_dir/${container_name}/init/init-lidarr-resource:/etc/s6-overlay/s6-rc.d/init-lidarr-resource \
 -v $base_data_dir/public/downloads:/downloads \
 -v $base_data_dir/public/:/data \
 --label 'traefik.http.routers.'${container_name}'.rule=Host(`'${container_name}.$domain'`)' \
