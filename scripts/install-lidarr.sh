25ff0f99b1a63957c5b34d6a25c3cb06fd13be1a
dezhishen <26274059+dezhishen@users.noreply.github.com>
2025-04-10 14:12:15 +0800

    fix:修改为复制

--- scripts/install-lidarr.sh
+++ scripts/install-lidarr.sh
@@ -14,8 +14,7 @@ echo """
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 if [ ! -f "/app/lidarr/bin/Localization/Core/zh.json/zh.json" ];then
-    ln -s /app/lidarr/bin/Localization/Core/zh.json/zh_CN.json /app/lidarr/bin/Localization/Core/zh.json > /dev/null
-    chown -h abc:abc /app/lidarr/bin/Localization/Core/zh.json > /dev/null
+    cp -f /app/lidarr/bin/Localization/Core/zh.json/zh_CN.json /app/lidarr/bin/Localization/Core/zh.json  
 fi
 """ > $base_data_dir/${container_name}/init.d/create-ln-zh-json
 chmod +x $base_data_dir/${container_name}/init.d/create-ln-zh-json
