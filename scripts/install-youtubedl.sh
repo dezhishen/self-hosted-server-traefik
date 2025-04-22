f0d9ac5943ffe5d97a0921e837c0835699158308
dezhishen <26274059+dezhishen@users.noreply.github.com>
2025-04-22 16:07:49 +0800

    feat:新增youtubedl

---
+++ scripts/install-youtubedl.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+domain=$1
+base_data_dir=$2
+docker_network_name=$3
+tls=$4
+container_name=youtubedl
+image=tzahi12345/youtubedl-material:latest
+port=17442
+
+docker run -d --name ${container_name} \
+--network=traefik --network-alias=${container_name} \
+--hostname=${container_name} \
+-v /docker_data/${container_name}/appdata:/app/appdata \
+-v /docker_data/public/youtube/audio:/app/audio \
+-v /docker_data/public/youtube/video:/app/video \
+-v /docker_data/public/youtube/subscriptions:/app/subscriptions \
+-v /docker_data/public/youtube/users:/app/users \
+-m 256M \
+-e UID=`id -u` \
+-e GID=`id -g` \
+-e use_local_db=true \
+-e ytdl_url=https://${container_name}.${domain} \
+--label "traefik.enable=true" \
+--label 'traefik.http.routers.'${container_name}'.rule=Host(`'${container_name}.$domain'`)' \
+--label "traefik.http.routers.${container_name}.tls=${tls}" \
+--label "traefik.http.routers.${container_name}.tls.certresolver=traefik" \
+--label "traefik.http.routers.${container_name}.tls.domains[0].main=*.$domain" \
+--label "traefik.http.services.${container_name}.loadbalancer.server.port=${port}" \
+${image}
