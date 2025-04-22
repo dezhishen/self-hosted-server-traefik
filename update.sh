fb5ae92bd05850481f5386cb210ea7f0f35d1fe9
dezhishen <26274059+dezhishen@users.noreply.github.com>
2025-04-22 11:10:11 +0800

    feat:增加docker登录

--- update.sh
+++ update.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+# check if login to docker
+# $HOME/.docker/config.json 是否存在
+mount_volumns=""
+if [ ! -f $HOME/.docker/config.json ]; then
+    # 是否登录
+    read -p "未登录docker hub, 是否登录? (y/n): " is_login
+    if [ "$is_login" == "y" ]; then
+        read -p "请输入docker hub账号: " docker_user
+        docker login --username $docker_user
+        if [ $? -ne 0 ]; then
+            echo "登录失败, 请检查网络或账号密码"
+        else
+            echo "登录成功"
+            mount_volumns="-v $HOME/.docker/config.json:/config.json"
+        fi
+    fi
+
+else
+    mount_volumns="-v $HOME/.docker/config.json:/config.json"
+fi
 docker run --name=watchtower --rm \
--d -v /var/run/docker.sock:/var/run/docker.sock \
+-d ${mount_volumns} -v /var/run/docker.sock:/var/run/docker.sock \
 --network=traefik containrrr/watchtower -c `cat /etc/DDSRem/container_name/*.txt` --run-once -x ` $@
