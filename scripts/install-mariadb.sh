9c5c3783f762d8a92793615278fe6459eb9b1fc6
dezhishen <26274059+dezhishen@users.noreply.github.com>
2025-04-27 11:15:17 +0800

    feat:补充mariadb备份程序的单次备份逻辑

--- scripts/install-mariadb.sh
+++ scripts/install-mariadb.sh
@@ -48,30 +48,50 @@ mariadb_container_name=${container_name}
 
 read -p "是否重装 备份程序 ? [y/n] " install_mariadb_backup
 if [ "$install_mariadb_backup" = "y" ]; then
+    container_name=mariadb-backup
+    image=databack/mysql-backup
     # 创建备份目录，避免权限问题
     if [ ! -d ${base_data_dir}/${container_name}/backup ]; then
         mkdir -p ${base_data_dir}/${container_name}/backup
     fi
     MYSQL_HOST=${mariadb_container_name}
     MYSQL_PASSWORD=$(`dirname $0`/get-args.sh MYSQL_PASSWORD "mysql密码" )
-    container_name=mariadb-backup
-    image=databack/mysql-backup
-    docker pull $image
-    docker stop $container_name > /dev/null
-    docker rm $container_name
-    docker run --restart=always -d \
-    --hostname=${container_name} \
-    --name ${container_name} -m 64M \
-    --user=`id -u`:`id -g` \
-    -e TZ=Asia/Shanghai \
-    -e LANG=zh_CN.UTF-8 \
-    -e DB_SERVER=${MYSQL_HOST} \
-    -e DB_USER=root \
-    -e DB_PASS=${MYSQL_PASSWORD} \
-    -e DB_DUMP_FREQUENCY=60 \
-    -e DB_DUMP_BEGIN=0000 \
-    -e DB_DUMP_TARGET=/db \
-    -v ${base_data_dir}/${container_name}/backup/:/db \
-    --network=${docker_network_name} --network-alias=${container_name} \
-    ${image} dump
+    read -p "是否单次备份[y/n]:" backup_once
+    if [ "$backup_once" = "y" ]; then
+        docker pull $image
+        docker stop $container_name-once > /dev/null
+        docker rm $container_name-once
+        docker run --rm -it \
+        --hostname=${container_name} \
+        --name ${container_name}-once -m 64M \
+        --network=${docker_network_name} --network-alias=${container_name} \
+        --user=`id -u`:`id -g` \
+        -e TZ=Asia/Shanghai \
+        -e LANG=zh_CN.UTF-8 \
+        -e DB_SERVER=${MYSQL_HOST} \
+        -e DB_USER=root \
+        -e DB_PASS=${MYSQL_PASSWORD} \
+        -e DB_DUMP_TARGET=/db \
+        -v ${base_data_dir}/${container_name}/backup/:/db \
+        ${image} dump --once
+    else
+        docker pull $image
+        docker stop $container_name > /dev/null
+        docker rm $container_name
+        docker run --restart=always -d \
+        --hostname=${container_name} \
+        --name ${container_name} -m 64M \
+        --user=`id -u`:`id -g` \
+        -e TZ=Asia/Shanghai \
+        -e LANG=zh_CN.UTF-8 \
+        -e DB_SERVER=${MYSQL_HOST} \
+        -e DB_USER=root \
+        -e DB_PASS=${MYSQL_PASSWORD} \
+        -e DB_DUMP_FREQUENCY=60 \
+        -e DB_DUMP_BEGIN=0000 \
+        -e DB_DUMP_TARGET=/db \
+        -v ${base_data_dir}/${container_name}/backup/:/db \
+        --network=${docker_network_name} --network-alias=${container_name} \
+        ${image} dump
+    fi
 fi
