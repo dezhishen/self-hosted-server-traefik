81a810c73bd3f59f4ee8903eff2d843de593b4e3
dezhishen <26274059+dezhishen@users.noreply.github.com>
2026-02-10 10:26:18 +0800

    feat:更新key获取逻辑

--- scripts/install-beszel-agent.sh
+++ scripts/install-beszel-agent.sh
@@ -10,7 +10,19 @@ port=45876
 docker pull $image
 
 `dirname $0`/stop-container.sh ${container_name}
-
+echo "尝试从beszel容器获取公钥"
+path=${base_data_dir}/beszel/data/id_ed25519.pub
+if [ -f "$path" ]; then
+    public_key=$(cat $path)
+    echo "获取到公钥: ${public_key}"
+fi
+if [ -z "$public_key" ]; then
+    read -p "未获取到公钥，请从beszel容器内的/beszel_data/id_ed25519.pub文件中获取公钥，并输入：" public_key
+    if [ -z "$public_key" ]; then
+        echo "公钥不能为空，退出"
+        exit 1
+    fi
+fi
 docker run -d --name=${container_name} \
 --restart=always \
 --network=$docker_network_name \
@@ -18,7 +30,7 @@ docker run -d --name=${container_name} \
 -e TZ="Asia/Shanghai" \
 -e LANG="zh_CN.UTF-8" \
 -e PORT=45876 \
--e KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBW46jEI5AIRIVTNYTk1bnCWXQplc0EtHxFfWMroSXE" \
+-e KEY="$public_key" \
 -m 64M \
 -v /var/run/docker.sock:/var/run/docker.sock:ro \
 --label 'traefik.http.routers.'${container_name}'.rule=Host(`'${container_name}.$domain'`)' \
